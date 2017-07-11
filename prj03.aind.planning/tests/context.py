7ea12a4aad17ffb8fba43d958ce21654e95f8efd
kostas oreopoulos <kostas.oreopoulos@gmail.com>
2017-07-11 23:24:20 +0300

    test: remove context.py from test files.

--- prj03.aind.planning/tests/context.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-parent = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
--- prj03.aind.planning/tests/test_my_air_cargo_problems.py
+++ prj03.aind.planning/tests/test_my_air_cargo_problems.py
@@ -1,4 +1,8 @@
-from .context import *
+import os
+import sys
+
+parent = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
 
 from aimacode.planning import Action
 from aimacode.utils import expr
